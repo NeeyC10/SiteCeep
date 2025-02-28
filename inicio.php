4ff00adaaf1f8d4966b11dce05341d03ce50a8ca
Admin <a>
2025-02-27 21:37:42 -0300

    site tcc primeira versão

---
+++ inicio.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cursos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="inicio.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row corfundo col-md-12">
+            <div class="col-md-2">
+                <div><img src="https://escolatecnicalaranjeiras.com.br/wp-content/uploads/2020/11/cropped-logo-em-pdf-ceep.png" class="img-fluid"></div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="text-center"><h2>CENTRO ESTADUAL DE EDUCAÇÃO PROFISSIONAL </h2></div><hr>
+                <div class="text-center">Profª. Naiana Babaresco Souza - Escola Técnica de Laranjeiras do Sul - PR</div>
+            </div>
+
+            <div class="col-md-2">
+                <div><img src="https://images.seeklogo.com/logo-png/23/2/brasao-do-parana-logo-png_seeklogo-235186.png" class="img-fluid"></div>
+            </div>
+        </div><br>
+
+
+        <div class="row">
+            <div class="col-md-3 p-0">
+                <div><button class="btn btn-secondary" type="button">inicio</button></div>
+            </div>
+
+            <div class="col-md-3">
+                <div><button class="btn btn-secondary" type="button">cursos</button></div>
+            </div>
+
+            <div class="col-md-3">
+                <div><button class="btn btn-secondary" type="button">noticias</button></div>
+            </div>
+            
+            <div class="col-md-3">
+                <div><button class="btn btn-secondary" type="button">contato</button></div>
+            </div>
+        </div><br>
+
+    <!-- Carrossel -->            
+        <div class="row">
+            <div class="img-fluid" col-md-8>
+                <img src="https://escolatecnicalaranjeiras.com.br/wp-content/uploads/2020/11/WhatsApp-Image-2020-11-18-at-09.27.26.jpeg" class="imgInicial">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="text-center"><h1>Sobre o Colégio</h1></div>
+            <div><p>Em 2008, dez municípios do Paraná foram selecionados para receber unidades do Centro Estadual de Educação Profissional (CEEP). Laranjeiras do Sul foi um dos municípios contemplados, devido à sua localização central, atendendo à demanda regional. O CEEP na região Cantuquiriguaçu foi planejado para atender áreas específicas como o maior assentamento da América Latina e a maior Terra Indígena do Paraná.
+
+Estudos de demanda foram realizados para justificar a necessidade da unidade de educação profissional, envolvendo alunos do Ensino Fundamental e Médio. Diversas reuniões foram realizadas com entidades municipais e secretarias de educação para discutir a oferta e demanda de cursos. Os cursos escolhidos foram: Técnico em Contabilidade, Técnico em Edificações, Técnico em Agronegócio e Técnico em Enfermagem.
+
+A construção do CEEP começou em 2011, mas foi interrompida em 2013 devido a problemas com o Departamento Nacional de Infraestrutura de Transporte (DNIT). Em 2016, a professora Eliane Maria da Silva foi nomeada a primeira diretora do centro. Com sua equipe, ela trabalhou arduamente para preparar a infraestrutura e iniciar as atividades.
+Em agosto de 2016, os primeiros cursos começaram a funcionar no período noturno. Em fevereiro de 2017, o primeiro curso Técnico em Agronegócio Integrado começou no período da manhã. A instituição tem capacidade para atender 1200 alunos, com uma área construída de 6.495,56 m² e uma área total de 14.354,15 m².
+
+Inicialmente chamado de "Centro Estadual de Educação Profissional de Laranjeiras do Sul", o nome foi mudado para homenagear a Professora Naiana Babaresco de Souza, uma servidora pública dedicada à educação e que enfrentou o câncer com determinação. A escolha do nome reflete um referencial jovem e serve como homenagem à servidora.</p></div>
+
+        </div>
+
+
+        <div class="row">
+            <div class=" col-md-12 corfundo">
+                <footer>
+                    footer
+                </footer>
+            </div>
+        </div>
+
+    </div>
+
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ noticias.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cursos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="noticia.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row corfundo col-md-12">
+            <div class="col-md-2">
+                <div><img src="https://escolatecnicalaranjeiras.com.br/wp-content/uploads/2020/11/cropped-logo-em-pdf-ceep.png" class="img-fluid"></div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="text-center">
+                    <h1>CENTRO ESTADUAL DE EDUCAÇÃO PROFISSIONAL </h1>
+                </div>
+                <hr>
+                <div class="text-center">Profª. Naiana Babaresco Souza - Escola Técnica de Laranjeiras do Sul - PR</div>
+            </div>
+
+            <div class="col-md-2">
+                <div><img src="https://images.seeklogo.com/logo-png/23/2/brasao-do-parana-logo-png_seeklogo-235186.png" class="img-fluid"></div>
+            </div>
+        </div><br>
+
+
+        <div class="row">
+            <div class="col-md-3 p-0">
+                <div><button class="btn btn-secondary" type="button">inicio</button></div>
+            </div>
+
+            <div class="col-md-3">
+                <div><button class="btn btn-secondary" type="button">cursos</button></div>
+            </div>
+
+            <div class="col-md-3">
+                <div><button class="btn btn-secondary" type="button">noticias</button></div>
+            </div>
+
+            <div class="col-md-3">
+                <div><button class="btn btn-secondary" type="button">contato</button></div>
+            </div>
+        </div><br><hr>
+
+
+
+        <div class="row">
+            <div><h1 class="text-center">Noticias</h1></div><br>
+            <div class="col-md-12 mt-3">
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="https://embratur.com.br/wp-content/uploads/2022/07/Embratur-Brasil-ultrapassa-marca-de-1-milhao-de-turistas-estrangeiros-recebidos-pela-primeira-vez-desde-2020-1.png" class="img-fluid " height="50px">
+                    </div>
+                    <div class="col-md-8 fundoResumoCursos">
+                    <div class="text-center"><h2>Noticia 1</h2></div>
+                    <div class="text-center"><p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></div>
+                    </div>
+                </div> <hr>
+
+                <div class="row mt-2">
+                    <div class="col-md-4">
+                        <img src="https://embratur.com.br/wp-content/uploads/2022/07/Embratur-Brasil-ultrapassa-marca-de-1-milhao-de-turistas-estrangeiros-recebidos-pela-primeira-vez-desde-2020-1.png" class="img-fluid" height="50px">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="text-center"><h2>Noticia 2</h2></div>
+                    <div class="text-center"><p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></div>
+                    </div>
+                </div> <hr>
+
+                <div class="row mt-2">
+                    <div class="col-md-4">
+                        <img src="https://embratur.com.br/wp-content/uploads/2022/07/Embratur-Brasil-ultrapassa-marca-de-1-milhao-de-turistas-estrangeiros-recebidos-pela-primeira-vez-desde-2020-1.png" class="img-fluid" height="50px">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="text-center"><h2>Noticia 3</h2></div>
+                    <div class="text-center"><p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></div>
+                    </div>
+                </div> <hr>
+
+                <div class="row mt-2">
+                    <div class="col-md-4">
+                        <img src="https://embratur.com.br/wp-content/uploads/2022/07/Embratur-Brasil-ultrapassa-marca-de-1-milhao-de-turistas-estrangeiros-recebidos-pela-primeira-vez-desde-2020-1.png" class="img-fluid" height="50px">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="text-center"><h2>Noticia 4</h2></div>
+                    <div class="text-center"><p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></div>
+                    </div>
+                </div> <hr>
+
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
