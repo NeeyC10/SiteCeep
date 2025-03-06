68c21e8e01821d7cfc4223ec269e38721d54a4c9
Admin <a>
2025-03-06 20:57:56 -0300

    segunda versao

---
+++ topo.php
@@ -0,0 +1,37 @@
+<div class="container">
+        <div class="row corfundo">
+            <div class="col-2">
+                <div><img src="https://escolatecnicalaranjeiras.com.br/wp-content/uploads/2020/11/cropped-logo-em-pdf-ceep.png" class="img-fluid"></div>
+            </div>
+
+            <div class="col-8">
+                <div class="text-center">
+                    <h1>CENTRO ESTADUAL DE EDUCAÇÃO PROFISSIONAL </h1>
+                </div>
+                <hr>
+                <div class="text-center">Profª. Naiana Babaresco Souza - Escola Técnica de Laranjeiras do Sul - PR</div>
+            </div>
+
+            <div class="col-2">
+                <div><img src="https://images.seeklogo.com/logo-png/23/2/brasao-do-parana-logo-png_seeklogo-235186.png" class="img-fluid"></div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-3 p-1">
+            <div><a href="inicio.php"><button class="btn btn-secondary" type="button">Início</button></a></div>   
+            </div>
+
+            <div class="col-3 p-1">
+                <div><a href="curso.php"><button class="btn btn-secondary" type="button">Cursos</button></a></div>
+            </div>
+
+            <div class="col-3 p-1">
+            <div><a href="noticias.php" ><button class="btn btn-secondary" type="button" >Notícias</button></a></div>
+            </div>
+            
+            <div class="col-3 p-1">
+            <div><a href="contato.php"><button class="btn btn-secondary" type="button">Contato</button></a></div>
+            </div>
+        </div><br><hr>
