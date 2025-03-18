b90ab43074ead3cc8c7195792afe18ae08e01cfc
Admin <a>
2025-03-17 21:31:29 -0300

    footer

--- footer.php
+++ footer.php
@@ -4,17 +4,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="footer.css">
     <title>Document</title>
 </head>
 
 <body>
-    <div class="row text-center">
-        <div class="col-4">a</div>
-        <div class="col-4">b</div>
-        <div class="col-4">c</div>
-    </div>
+<div class="row text-center footer">
+        <div class="col-4 mt-4 mb-4">
+            <div class="row mt-5">
+                <div class="col-3"></div>
+                <div class="col-2 mt-4 mb-4">
+                    <a href="https://www.instagram.com/ceeplaranjeiras/">
+                        <img src="img/instagram.png">
+                    </a>
+                </div>
+                <div class="col-2 mt-4 mb-4">
+                    <a href="https://www.facebook.com/ceeplaranjeirasdosul/">
+                        <img src="img/facebook.png" alt="">
+                    </a>
+                </div>
+                <div class="col-2 mt-4 mb-4">
+                    <a href="ljsceeplaranjeiras@seed.pr.gov.br">
+                        <img src="img/email.png" alt="">
+                    </a>
+                </div>
+                <div class="col-3"></div>
+            </div>
+        </div>
 
+        <div class="col-4 mt-4 mb-4"><br>
+            <div class="mt-4 mb-4">(42) 3635-2397 <br> R. Cel. Guilherme de Paula <br> 1937-1967 <br> Centro, Laranjeiras do Sul - PR <br> 85301-404 </div>
+        </div>  
+
+        <div class="col-4 mt-4 mb-4">
+            <div class="row text-center">
+                <div class="row text-center"><p> Horários <br> Segunda-Feira à Sexta- Feira <br> 7:30 às 11:50 <br> 18:30 às 23:00 <br> Sábados e Domingos <br> Fechado</p></div>
+            </div>
+
+        </div>
+        <hr>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- topo.php
+++ topo.php
@@ -1,11 +1,11 @@
-<div class="containr">
+
 
     <!-- Header para pc -->
     <div class="d-none d-sm-block">
         <div class="row corfundo">
             <div class="col-1"></div>
             <div class="col-2 mt-3">
-                <div><img src="https://escolatecnicalaranjeiras.com.br/wp-content/uploads/2020/11/cropped-logo-em-pdf-ceep.png"  height="230px"></div>
+                <div><img src="https://escolatecnicalaranjeiras.com.br/wp-content/uploads/2020/11/cropped-logo-em-pdf-ceep.png" height="230px"></div>
             </div>
 
             <div class="col-6">
@@ -32,63 +32,53 @@
             <div class="col-3">
                 <div><img src="https://escolatecnicalaranjeiras.com.br/wp-content/uploads/2020/11/cropped-logo-em-pdf-ceep.png" class="img-fluid"></div>
             </div>
-            <div class="col-4 text-center mt-4"> <h1>CEEP</h1> </div>
+            <div class="col-4 text-center mt-4">
+                <h1>CEEP</h1>
+            </div>
             <div class="col-3">
                 <div><img src="https://images.seeklogo.com/logo-png/23/2/brasao-do-parana-logo-png_seeklogo-235186.png" class="img-fluid"></div>
             </div>
             <div class="col-1"></div>
 
-            
-                <hr>
 
-                <div class="text-center">
-                    <h6 class="fs-6">Profª. Naiana Babaresco Souza - Escola Técnica de Laranjeiras do Sul - PR</h6>
-                </div>
+            <hr>
+
+            <div class="text-center">
+                <h6 class="fs-6">Profª. Naiana Babaresco Souza - Escola Técnica de Laranjeiras do Sul - PR</h6>
             </div>
-
-
         </div>
+
+
     </div>
+</div>
 
 
 
 <!--Botoes-->
 
-    <div class="row mt-2 m-3">
+<div class="row mt-2 m-3">
 
-        <div class="col-3 p-1">
-            <div><a href="inicio.php"><button class="btn btn-secondary" type="button">Inicio</button></a></div>
-        </div>
-
-        <div class="col-3 p-1">
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Cursos</button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a class="dropdown-item" href="cursosSubsequentes.php">Subsequentes</a></li>
-                    <li><a class="dropdown-item" href="cursosIntegrados.php ">Integrados</a></li>
-                </ul>
-            </div>
-        </div>
-
-        <div class="col-3 p-1">
-            <div><a href="noticias.php"><button class="btn btn-secondary" type="button">Notícias</button></a></div>
-        </div>
-
-        <div class="col-3 p-1">
-            <div><a href="contato.php"><button class="btn btn-secondary" type="button">Contato</button></a></div>
-        </div>
-
-        <!-- <div class="col-3 p-1">
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Inicio</button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a class="dropdown-item" href="#">...</a></li>
-                    <li><a class="dropdown-item" href="#">...</a></li>
-                </ul>
-            </div>
-        </div>
--->
-        
-
+    <div class="col-3 p-1">
+        <div><a href="inicio.php"><button class="btn btn-secondary" type="button">Inicio</button></a></div>
     </div>
-    <hr>
+
+    <div class="col-3 p-1">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Cursos</button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="cursosSubsequentes.php">Subsequentes</a></li>
+                <li><a class="dropdown-item" href="cursosIntegrados.php ">Integrados</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-3 p-1">
+        <div><a href="noticias.php"><button class="btn btn-secondary" type="button">Notícias</button></a></div>
+    </div>
+
+    <div class="col-3 p-1">
+        <div><a href="contato.php"><button class="btn btn-secondary" type="button">Contato</button></a></div>
+    </div>
+
+</div>
+<hr>
